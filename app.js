521419a6036e383899d6f2fd49684f9608e4acc9
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-01-18 07:11:58 +0000

    added app.use, app.get to root

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', function(req, res) {
+    var fName = req.body.firstName;
+    var sName = req.body.secondName;
+    var email = req.body.email;
+    console.log(fName, sName, email);
+})
+
 app.listen(port, function(){
     console.log("Server running on port 3000");
 })
