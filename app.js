838c2dcef50d250204b0787a549fe47d866280c8
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-01-19 07:00:13 +0000

    added data array, https request

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
+const https = require('https');
 const app = express();
 const port = 3000;
 
@@ -12,12 +13,45 @@ app.get('/', function (req, res){
 });
 
 app.post('/', function(req, res) {
-    var fName = req.body.firstName;
-    var sName = req.body.secondName;
-    var email = req.body.email;
-    console.log(fName, sName, email);
+    const fName = req.body.firstName;
+    const sName = req.body.secondName;
+    const email = req.body.email;
+    
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: fName,
+                    LNAME: sName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us7.api.mailchimp.com/3.0/lists/6e2bbc0354";
+    const options = {
+        method: "POST",
+        auth: "mashu:55f8883e6a48605fc7cafe0a6282aeaa-us7"
+
+    }
+
+    https.request(url, options, function(response) {
+        response.on("data", function(data) {
+            console.log(JSON.parse(data));
+        })
+    })
+
 })
 
 app.listen(port, function(){
     console.log("Server running on port 3000");
 })
+
+//API KEY 
+// 55f8883e6a48605fc7cafe0a6282aeaa-us7
+
+//LIST ID
+// 6e2bbc0354
